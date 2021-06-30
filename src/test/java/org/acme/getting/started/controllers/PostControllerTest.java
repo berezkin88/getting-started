e48040196e2361caaafa1b4a6c12ccc0fac2ade6
berezkin88 <berezkin05@gmail.com>
2021-06-30 23:44:59 +0300

    finished controller tests

--- src/test/java/org/acme/getting/started/controllers/PostControllerTest.java
+++ src/test/java/org/acme/getting/started/controllers/PostControllerTest.java
@@ -3,14 +3,16 @@ package org.acme.getting.started.controllers;
 import io.quarkus.test.Mock;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
+import io.restassured.http.ContentType;
 import org.acme.getting.started.models.Post;
 import org.acme.getting.started.services.PostService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import javax.enterprise.context.ApplicationScoped;
 
 import static io.restassured.RestAssured.given;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @QuarkusTest
 class PostControllerTest {
@@ -23,48 +25,66 @@ class PostControllerTest {
     @Test
     void getAllPosts() {
         given()
-            .when().get("/posts")
+            .when()
+            .get("/posts")
             .then()
             .statusCode(200);
 
-        Mockito.verify(postService, Mockito.times(1)).findAllPosts();
+        verify(postService, times(1)).findAllPosts();
     }
 
     @Test
     void getPost() {
-        given()
-            .when().get("/posts/1")
-            .then()
-            .statusCode(204);
+        when(postService.findPostById(1L)).thenReturn(post);
 
-        Mockito.verify(postService, Mockito.times(1)).findPostById(1L);
+        given()
+            .when()
+            .get("/posts/1")
+            .then()
+            .statusCode(200);
+
+        verify(postService, times(1)).findPostById(1L);
     }
 
     @Test
     void createPost() {
-        // research
         given()
-            .when().post("/posts")
+            .contentType(ContentType.JSON)
+            .body(new Post())
+            .when()
+            .post("/posts")
             .then()
             .statusCode(200);
 
-        Mockito.verify(postService, Mockito.times(1)).creatPost(null);
+        verify(postService, times(1)).creatPost(any(Post.class));
     }
 
     @Test
     void updatePost() {
+        given()
+            .contentType(ContentType.JSON)
+            .body(new Post())
+            .when()
+            .put("/posts")
+            .then()
+            .statusCode(202);
+
+        verify(postService, times(1)).updatePost(any(Post.class));
     }
 
     @Test
     void deletePost() {
+        given()
+            .when()
+            .delete("/posts/1")
+            .then()
+            .statusCode(204);
+
+        verify(postService, times(1)).deletePost(1L);
     }
 
     @Mock
     @ApplicationScoped
     public static class PostServiceBean extends PostService {
-        @Override
-        public Post findPostById(Long id) {
-            return post;
-        }
     }
 }
